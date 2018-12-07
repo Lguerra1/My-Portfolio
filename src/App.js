4996b048b45fd933d74264189bcc23612e0519b9
maxjacobson <max.jacobson@torchlms.com>
2018-12-07 14:50:30 -0700

    attempt to push to master

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Larry is so awesome!
+            Larry is so awesome! ORALE
           </p>
           <a
             className="App-link"
