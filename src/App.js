e88ea960fd7b1ae94f786ca394142eb3426868df
maxjacobson <max.jacobson@torchlms.com>
2018-12-07 14:48:57 -0700

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Max is so awesome!
+            Larry is so awesome!
           </p>
           <a
             className="App-link"
