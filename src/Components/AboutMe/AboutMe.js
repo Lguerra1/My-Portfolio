dcb44a6b51c40385c520338c78442dbce9c713d0
unknown <Nightmachine@gmail.com>
2019-08-11 18:30:15 -0600

    fixed skills page

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -9,27 +9,16 @@ const AboutMe = () => {
         <container id="aboutlink">
             <h1 className={`text-center display-3 mb-4 ${classes.aboutMeContainer}`} >About me</h1>
             <div class="row">
-                <div class="col-2">
+                <div class="col-1">
                 </div>
-                <div className={`${classes.biography} col-8`}>
+                <div className={`${classes.biography} col-10`}>
                     <p>One of my favorite quotes is,"you reap what you sow." This has been true throughout my entire life, as I come from a farming background. Ever since I was seven-years-old, my parents have taught me to work hard and to learn that hard is necessary to succeed.</p>
                     <p>In my free time, I like to train martial arts. I have been training in Brazilian Jujitsu and Boxing for the past 6 years and have earned a blue belt in Four-7 Combat Jujitsu. Training in martial arts has taught me to always stay humble and to never underestimate others or get too confident.
                     </p>
                 </div>
-                <div class="col-2">
+                <div class="col-1">
                 </div>
             </div>
-
-            {/* <div className={`${classes.aboutMeContainer}`} id="aboutlink">
-                <div className={classes.biography}>
-                    <p>
-                        I am new to the field of web development.  I recently graduated from DevMountain with a focus on the PERN stack and a solid understanding of functional programming. I have had the opportunity to build a couple of web applications (including this site).
-            </p>
-                    <p className={`text-center mb-5`}>
-                        One of my favorite quotes is,"you reap what you sow."  This has been true throughout my entire life, as I come from a farming background. Ever since I was seven-years-old, my parents have taught me to work hard and to learn that hard is necessary to succeed. In my free time, I like to train martial arts. I have been training in Brazilian Jujitsu and Boxing for the past eight years and have earned a blue belt in Four-7 Combat Jujitsu. Training in martial arts has taught me to always stay humble and to never underestimate others or get too confident.
-                    </p>
-                </div>
-            </div> */}
         </container>
     )
 }
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import classes from './Skills.module.css'
-import css from '../../image/CSS.jpg'
-import html5 from '../../image/html5.jpg'
+import css from '../../image/CSS.png'
+import html5 from '../../image/html5.PNG'
 import Js from '../../image/Js.jpg'
 import postgres from '../../image/postgres.jpg'
-import redux from '../../image/Redux.jpg'
-import react from '../../image/React.jpg'
+import redux from '../../image/Redux.png'
+import react from '../../image/React.png'
 import node from '../../image/node.jpg'
 import Semantic from '../../image/Semantic.jpg'
+import Bootstrap from '../../image/Bootstrap.jpg'
+import Wordpress from '../../image/Wordpress.png'
 
 
 const Skills = () => {
 
     return (
 
-        <container id="skillslink">
-            <h1 className={`mt-5 mb-5 text-center display-3`}>Skills</h1>
+        <container >
+            <h1 id="skillslink" className={`mt-5 mb-5 text-center display-3 ${classes.title}`}>Skills</h1>
             <div className="row">
                 <div className="col text-center mt-4">
                     <img className={`${classes.img} text-center`} src={html5} />
@@ -29,13 +31,16 @@ const Skills = () => {
                 <div className="col text-center mt-4">
                     <img className={`${classes.img} text-center`} src={postgres} />
                 </div>
+                <div className="col text-center mt-4">
+                    <img className={`${classes.img} text-center`} src={Wordpress} />
+                </div>
             </div>
             <div className="row">
                 <div className="col text-center mt-4">
                     <img className={`${classes.img} text-center`} src={redux} />
                 </div>
                 <div className="col text-center mt-4">
-                    <img className={`${classes.img} text-center`} src={react} />
+                    <img className={`${classes.img} text-center `} src={react} />
                 </div>
                 <div className="col text-center mt-4">
                     <img className={`${classes.img} text-center`} src={node} />
@@ -43,27 +48,11 @@ const Skills = () => {
                 <div className="col text-center mt-4">
                     <img className={`${classes.img} text-center`} src={Semantic} />
                 </div>
+                <div className="col text-center mt-4">
+                    <img className={`${classes.img} text-center`} src={Bootstrap} />
+                </div>
             </div>
         </container>
-
-        // <div id="skillslink" className={classes.skillsContainer}>
-        //     <h1 className={`${classes.skills}, mt-5 text-center display-3`}>Skills</h1>
-        //     <div className={classes.skillsIcons}>
-        //         <div className={classes.rowOne}>
-        //             <img className={classes.img} src={html5} />
-        //             <img className={classes.img} src={css} />
-        //             <img className={classes.img} src={Js} />
-        //             <img className={classes.img} src={postgres} />
-        //         </div>
-        //         <div className={classes.rowTwo}>
-        //             <img className={classes.img} src={redux} />
-        //             <img className={classes.img} src={react} />
-        //             <img className={classes.img} src={node} />
-        //             <img className={classes.img} src={Semantic} />
-        //         </div>
-        //     </div>
-        // </div>
-
     )
 
 }
